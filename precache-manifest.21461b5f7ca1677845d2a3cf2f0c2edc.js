abf0f7dc5f4f7f6eb837702ec108f3eb00caebd2
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-05-19 20:07:43 +0100

    Updates

---
+++ precache-manifest.21461b5f7ca1677845d2a3cf2f0c2edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936b7c30ce0626a13585f27a94226bec",
+    "url": "/github-user-finder/index.html"
+  },
+  {
+    "revision": "17392b89fa0160627eaf",
+    "url": "/github-user-finder/static/css/main.b906ab75.chunk.css"
+  },
+  {
+    "revision": "bd10148cbf198aa8258c",
+    "url": "/github-user-finder/static/js/2.d621589d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/github-user-finder/static/js/2.d621589d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17392b89fa0160627eaf",
+    "url": "/github-user-finder/static/js/main.d569a50c.chunk.js"
+  },
+  {
+    "revision": "cbc27c34eafa2fa8160e",
+    "url": "/github-user-finder/static/js/runtime-main.b85a267c.js"
+  }
+]);
