9d6e358f8a039cb3e6ce0dc8b1ab1b7df9477b46
Pedro Silva <pedrosilvavpsn@gmail.com>
2020-06-05 22:33:07 +0100

    reducer and action

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import axios from 'axios';
+
+import GithubState from './context/github/githubState';
+
 import './App.css';
 
 const App = () => {
@@ -59,6 +62,7 @@ const App = () => {
    };
 
   return (
+    <GithubState>
     <Router>
     <div className='App'>
       <Navbar title="GitHub User Finder" icon="fab fa-github"/>
@@ -85,6 +89,7 @@ const App = () => {
       </div>
     </div>
     </Router>
+    </GithubState>
   );
 };
 export default App;
